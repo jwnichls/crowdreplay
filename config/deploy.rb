6c25f9560a6e1b2655f0d9a6c25ca1c9a00d3d8c
Jeffrey Nichols <jeff@jeffreynichols.com>
2014-06-19 13:57:37 -0400

    Fixes for Capistrano 3 and dotenv

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,3 @@
-require "dotenv/capistrano"
-
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
@@ -25,7 +23,7 @@ set :deploy_to, "/home/jwnichls/web/crowdreplay"
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{.env}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
